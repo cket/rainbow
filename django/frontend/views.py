2db35a07382260a8951e7c474e5a504a6bba6f1f
cket <cketchum@ucsc.edu>
2016-03-11 01:18:10 -0800

    Weather api was broken, oops. Fixed now with country code and API key :)

--- django/frontend/views.py
+++ django/frontend/views.py
@@ -11,16 +11,23 @@ from django.http import HttpResponse
 def index(request):
     #call script here
     reader = geoip2.database.Reader('./frontend/GeoLite2-City.mmdb')
-    ip = urlopen('http://ip.42.pl/raw').read().decode("utf-8") 
-    response = reader.city(ip) #ip
-    city = response.city.name
-    current_weather = requests.get('http://api.openweathermap.org/data/2.5/weather?q={}'.format(city))
-    lat = response.location.latitude
-    lon = response.location.longitude
 
-    good_weather = False
-    if 'rain' in current_weather.json():
-        good_weather = True
+    ip = urlopen('http://ip.42.pl/raw').read().decode("utf-8") 
+
+    response = reader.city(ip) #ip
+    lat = response.location.latitude
+    lon = response.location.longitude 
+    city = response.city.name
+
+    current_weather = requests.get('http://api.openweathermap.org/data/2.5/weather?q={},us&APPID=ddbf6e1389436b869677797efc0b2907'.format(city))
+    current_weather = current_weather.json()
+    weather_desc = current_weather['weather'][0]['description']
+
+    try:
+        if 0 < float(current_weather['rain']['3h']):  # free tier api includes only last 3 hours of weather data
+            good_weather = True
+    except KeyError:
+        good_weather = False # API sometimes leaves out the rain field if it isn't raining. but not always. cool.
 
     def rainbowFile(string):
         return os.path.isfile("../backend/"+string) and string.startswith('rainbow')
@@ -29,7 +36,7 @@ def index(request):
     to_execute="python3" if runnable.endswith(".py") else ""
 
     solar_vector = check_output("{} ../backend/{} {} {} {}".format(to_execute, runnable, int(time.time()), lat, lon), shell=True)
-    solar_vector = solar_vector.decode('utf-8').split(" ", 1)
+    solar_vector = solar_vector.decode('utf-8').split(" ", 1)  # our vector is read in as a byte string
     zenith_distance = list(map(float, solar_vector))[0]
     solar_height = 90 - zenith_distance
 
@@ -37,7 +44,7 @@ def index(request):
     if 0 < solar_height < 42 and good_weather:
         rainbow = "There could be a rainbow!"
     else:
-        rainbow = "There is definitely probably not a rainbow :("
+        rainbow = "There is probably not a rainbow :("
 #     rainbow+="""
 #                       ___----~~~~~~----___\n
 #                  _-~~~____-----~~-----____~~~-_\n
@@ -58,5 +65,5 @@ def index(request):
 #  \___________/________________________________________________________\n
 #  """
 
-    return HttpResponse("You are in {} at coordinates ({}, {}) with ip {}. {}: \
-        the solar height is {}".format(city, lat, lon, ip, rainbow, solar_height))
+    return HttpResponse("You are in {} at coordinates ({}, {}) with ip {} and your weather is {}. The solar height is {}. \
+        {}".format(city, lat, lon, ip, weather_desc, solar_height, rainbow))
